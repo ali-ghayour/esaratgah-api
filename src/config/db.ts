9210fb19bd96462990fca64916572de5a3deebec
ali-ghayour <ali.ghayour.se@gmail.com>
2024-10-29 01:46:06 +0330

    fix db.ts config

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const DB_URI = process.env.DB_URI || "mongodb://localhost:27017/your_database";
+const DB_URI = process.env.DB_URI as string;
 
 // Create a single MongoDB connection instance
 const connection = mongoose.createConnection(DB_URI);
