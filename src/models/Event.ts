60d1945231c10f1b80e606dd63f2b509d841e0ee
Ali Ghayour <ali.ghayour.g@gmail.com>
2024-11-12 21:41:19 +0330

    Event model added

---
+++ src/models/Event.ts
@@ -0,0 +1,24 @@
+import { Schema } from "mongoose";
+import { connection, autoIncrement } from "../config/db";
+import User from "./User";
+
+const EventSchema = new Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    slug: { type: String, required: true, unique: true },
+    description: { type: String, required: true },
+    participated_users: [
+      { type: Number, reqired: true, unique: true, ref: User },
+    ],
+    invited_users: [{ type: Number, reqired: true, unique: true, ref: User }],
+    created_by: { type: Number, required: true, ref: User },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+EventSchema.plugin(autoIncrement.plugin, "Event");
+
+const Event = connection.model("Event", EventSchema);
+export default Event;
