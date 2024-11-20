e03014aacec2ae74558eba64065f45ca3151b2bb
Ali Ghayour <ali.ghayour.g@gmail.com>
2024-11-20 22:55:32 +0330

    minor fix on error handler

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction, ErrorRequestHandler } from "express";
+import { Request, Response, NextFunction } from "express";
 import { CustomError } from "../helpers/CustomError";
 import { IResponse } from "../types";
 
